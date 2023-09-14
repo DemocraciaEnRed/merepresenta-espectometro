43b319c334d5da23c5466beb1b4a22b59f354cc0
xtian7489 <xtian7489@gmail.com>
2023-09-14 11:01:05 -0300

    change port

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 5000,
   },
   base: './',
   plugins: [react(),svgr()],
